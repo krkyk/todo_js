a14bb56f57cd13ebd05d51ee2575339854ec0915
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-05 21:04:28 +0900

    TODO追加機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの処理
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグ作成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  // spanタグと表示テキスト作成
+  const span = document.createElement("span");
+  span.innerText = inputText;
+
+  // buttonタグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // liタグの子要素に各要素を入れる
+  li.appendChild(span);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-btn")
+  .addEventListener("click", () => onClickAdd());
